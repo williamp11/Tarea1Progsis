1d62e58b637d252277864179b72f25cb3675fc21
WilliamP11 <willpaca@espol.edu.ec>
2025-11-14 22:57:15 -0500

    Update usuarios.h

--- usuarios.h
+++ usuarios.h
@@ -1,1 +1,16 @@
+#ifndef USUARIOS_H
+#define USUARIOS_H
+#include <stdbool.h>
 
+// Estructura para los usuarios
+typedef struct {
+    char usuario[20];
+    char clave[20];
+    char nombre[50];
+} Vendedor;
+
+// Declaración de funciones
+void cargar_vendedores(Vendedor **lista, int *count);
+bool verificar_credenciales(Vendedor *lista, int count, const char *user, const char *pass);
+
+#endif
